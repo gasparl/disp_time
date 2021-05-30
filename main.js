deef286224bb470d985b82f9bdd543b3c87c5698
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2021-05-30 09:46:47 +0200

    date

--- main.js
+++ main.js
@@ -438,8 +438,7 @@ function neat_date() {
         ("0" + (m.getMonth() + 1)).slice(-2) + "" +
         ("0" + m.getDate()).slice(-2) + "_" +
         ("0" + m.getHours()).slice(-2) + "" +
-        ("0" + m.getMinutes()).slice(-2) + "" +
-        ("0" + m.getSeconds()).slice(-2);
+        ("0" + m.getMinutes()).slice(-2);
 }
 
 function shuffle(arr) {
