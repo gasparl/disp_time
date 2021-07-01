2e86b2acae2825d68c73e83491ee8f9288954ff3
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2021-07-01 16:56:28 +0200

    psychopy

--- py_version.py
+++ py_version.py
@@ -1,39 +1,115 @@
+from psychopy.visual import Window, Rect, TextStim
+from psychopy.core import wait, quit, Clock
 from psychopy.hardware import keyboard
-from psychopy import visual, core, event
-mywin = visual.Window([600, 400])
-myrect = visual.Rect(mywin, 1.8, 1.8, fillColor = "red")
-kb = keyboard.Keyboard()
-event.globalKeys.add(key='q', modifiers= 'ctrl', func=core.quit)
+from time import gmtime, strftime, sleep
+from sys import exit
 
-backg = "white" # "black" / "white"
-
-times = 10
-durs = [16, 50, 150, 300, 500]
-
-###
-if backg == "white":
-    stimcolor = 'black'
+bg_color = 'white' # white black
+#
+if bg_color == 'white':
+    stim_color = 'black'
+elif bg_color == 'black':
+    stim_color = 'white'
 else:
-    stimcolor = 'white'
-allstims = []
-for i in range(times):
-    for dur in durs:
-        allstims.append(dur)
+    exit('wrong bg_color')
 
-lasttime = 0
-timer = core.Clock()
+kb = keyboard.Keyboard()
+my_win = Window(fullscr = True, units = 'pix', color= 'white')
+
+therect = Rect( my_win, width=800, height=400, lineColor = 'lightblue', fillColor = 'red')
+info = TextStim(my_win, text ='Info...', height = 50, pos=(0, 0))
+
+durations = [16, 50, 150, 300, 500]
+date_time = str(strftime("%Y_%m%d_%H%M", gmtime()))
+data_out = open('disptime_psychopy_' + bg_color + '_' + date_time + '.txt', 'a', encoding='utf-8')
+
+current_stim = {}
+input_time = 'NA'
+time_start = 'NA'
+time_end = 'NA'
+        
+data_out.write('\t'.join([
+    "datetime",
+    "trial_number",
+    "stimulus",
+    "type",
+    "duration",
+    "timer",
+    "js_input",
+    "js_start_nextline",
+    "js_end_nextline",
+    "js_start_stamp",
+    "js_end_stamp",
+    "js_start_other",
+    "js_end_other"
+]) + '\n')
+def store_trial():     
+    data_out.write('\t'.join([
+        date_time,
+        trialnum,
+        '_',
+        'py',
+        current_stim['duration'],
+        'py',
+        pressed_key.rt,
+        time_start,
+        time_end,
+        time_start,
+        time_end,
+        'NA',
+        'NA'
+    ]) + '\n');
+
+def waitKeys(keyList = None):
+    kb.clock.reset()
+    while True:
+        keys = kb.getKeys(keyList=keyList)
+        if keys:
+            return keys[1]
+        sleep(0.00001)
+
+def disp_text():
+    info.text = 'Current trial: <b>' + str(trialnum) + '</b> (' + str(len(durations)-trialnum)  + ' left)\nDuration: <b>' + str(current_stim['duration']) + '</b>\nBackground: <b>' + bg_color + '</b>'
+
+def t_start():
+    global time_start
+    time_start = timer.getTime()
+def t_end():
+    global time_end
+    time_end = timer.getTime()
+
+trialnum = 0
+therect.draw()
+my_win.flip()
+waitKeys('x')
+timer = Clock()
+
+for dur in durations:    
+    trialnum += 1
+    current_stim['duration'] = dur
+    therect.fillColor = stim_color
+    therect.draw()
+    my_win.callOnFlip(t_start)
+    pressed_key = waitKeys('q')
     
-#    mywin.callOnFlip(onflp)
+    my_win.flip()
+    
+    wait((dur-10)/1000)
+    therect.fillColor = bg_color
+    therect.draw()
+    my_win.callOnFlip(t_end)
+    
+    my_win.flip()
+    
+    store_trial()
+    print( pressed_key.name, pressed_key.rt )
 
-for duration in allstims:
-    core.wait(0.5)
-    myrect.fillColor = "white"
-    myrect.draw()
-    mywin.flip()
-    core.wait(duration)
-    myrect.fillColor = "black"
-    myrect.draw()
-    mywin.flip()    
+data_out.close()
+wait(3)
+therect.fillColor = 'blue'
+therect.draw()
+my_win.flip()
+waitKeys()
 
-mywin.close()
-core.quit()
+quit()
+
